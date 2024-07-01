2db4fdbcec4cd0c59f4342bcf72bbc32dc6859c1
santhosh960041 <santhoshar2101@gmail.com>
2024-07-01 11:58:12 +0530

    Create sandy.java

---
+++ sandy.java
@@ -0,0 +1,9 @@
+class sandy
+  public static void main(string[]args)
+  {
+    System.out.println("hello");
+     System.out.println("welcome");
+     System.out.println("To");
+     System.out.println("devops");
+     System.out.println("class");
+  }
