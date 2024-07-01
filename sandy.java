868ea682af0e4f19129add7119deb3e8c12b6cee
santhosh960041 <santhoshar2101@gmail.com>
2024-07-01 12:22:49 +0530

    Update sandy.java

--- sandy.java
+++ sandy.java
@@ -1,5 +1,5 @@
-class sandy
-  public static void main(string[]args)
+class sandy{
+  public Static void main(String[]args)
   {
     System.out.println("hello");
      System.out.println("welcome");
@@ -7,3 +7,4 @@ class sandy
      System.out.println("devops");
      System.out.println("class");
   }
+}
